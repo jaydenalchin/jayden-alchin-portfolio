fb0eb53d86630f3a69f1f3aa0dd961b42096d22e
Jayden Alchin [SSW] <75593567+jaydenalchin@users.noreply.github.com>
2025-04-06 19:49:10 +1000

    💄 Applying 2025 rebrand

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,12 +2,13 @@
 	<title>Jayden Alchin</title>
 </svelte:head>
 
-<section class="bg-primary flex w-screen h-screen justify-center items-center">
-    <header class="text-white flex flex-col justify-center items-center text-center m-4">
+<section class="bg-cream flex w-screen h-screen justify-center items-center overflow-hidden">
+    <header class="text-black flex flex-col justify-center items-center text-center m-4 z-10">
         <h1 class="leading-tight">Jayden Alchin</h1>
-        <p class="text-opacity-90 text-3xl max-md:text-xl font-light self-center mt-4">UI/UX Designer, Digital Artist, Coffee Addict</p>
-        <div class="text-white text-3xl max-md:text-xl font-bold whitespace-nowrap mt-24 px-24 py-8 max-md:px-8 max-md:py-4 rounded-2xl max-md:rounded-lg border-4 border-white">
+        <p class="text-opacity-90 text-3xl max-md:text-xl font-medium self-center mt-4">UI/UX Designer, Digital Artist, Coffee Addict</p>
+        <div class="text-black text-3xl max-md:text-xl font-bold blackspace-nowrap mt-24 px-24 py-8 max-md:px-8 max-md:py-4 rounded-2xl max-md:rounded-lg border-4 border-black">
             In Development
         </div>
     </header>
+    <div class="absolute w-3/5 h-1/5 bottom-30 md:bottom-40 md:w-3/5 md:h-2/4 bg-latte rounded-[20%_80%_40%_60%/25%_75%_25%_75%] animate-blob"></div>
 </section>
